a9a39767a40fe32a54e05b9987df4ac2a369d1ca
Michael Pearson <mpearson2516@yahoo.com>
2021-03-17 14:32:30 -0700

    Created tables in DB and Spring Boot project

---
+++ DB/QuickDBD-My Doctor Appointments.sql
@@ -0,0 +1,54 @@
+﻿
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    email VARCHAR NOT NULL,
+    profilePic BYTEA,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    gender VARCHAR,
+    phone INT,
+    address INT REFERENCES address(address_id),
+    user_role INT REFERENCES roles(role_id),
+    dob DATE NOT NULL,
+    user_password VARCHAR NOT NULL
+);
+
+CREATE TABLE roles (
+    role_id INT PRIMARY KEY,
+    user_role VARCHAR NOT NULL
+);
+
+CREATE TABLE address (
+    address_id INT PRIMARY KEY,
+    street VARCHAR NOT NULL,
+    city VARCHAR NOT NULL,
+    state VARCHAR NOT NULL,
+    zipcode INT NOT NULL,
+    apt VARCHAR NOT NULL
+);
+
+CREATE TABLE bill (
+    invoice_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES users(user_id),
+    total NUMERIC NOT NULL,
+    status VARCHAR NOT NULL,
+    today DATE NOT NULL,
+    due_date DATE NOT NULL
+);
+
+CREATE TABLE appointment (
+    appointment_id SERIAL PRIMARY KEY,
+    schedule TIMESTAMP,
+    patient INT REFERENCES users(user_id),
+    doctor INT NOT NULL REFERENCES users(user_id),
+    status VARCHAR NOT NULL
+);
+
+CREATE TABLE "patient_doctor" (
+    patient INT NOT NULL REFERENCES users(user_id),
+    doctor INT NOT NULL REFERENCES users(user_id),
+    CONSTRAINT pk_patient_doctor PRIMARY KEY (
+        patient, doctor
+     )
+);
+
