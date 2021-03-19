c1139f07eac065facb2673e9f281c073e80cc52d
Michael Pearson <mpearson2516@yahoo.com>
2021-03-18 17:33:33 -0700

    Finished UsersRepository

--- App/Das/src/main/java/com/revature/repository/UsersRepository.java
+++ App/Das/src/main/java/com/revature/repository/UsersRepository.java
@@ -1,12 +1,23 @@
 package com.revature.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.revature.model.Users;
 
 public interface UsersRepository extends JpaRepository<Users, Integer> {
 
 	// Adding a new patient or doctor
+	// Updating existing patient or doctor
 	<S extends Users> S save(Users user);
+	
+	// Grab the user
+	 Users findByUserId(int userId);
+	 
+	 // Get all doctors
+	 @Query(value = "SELECT u FROM Users u WHERE u.roles.role = 'doctor'")
+	 List<Users> findAllByRoles();
 	
 }
