bb1e3b7bddd6b497de766db204070dd59cc2b752
Michael Pearson <mpearson2516@yahoo.com>
2021-03-18 18:58:23 -0700

    Created the BillRepository

--- App/Das/src/main/java/com/revature/repository/UsersRepository.java
+++ App/Das/src/main/java/com/revature/repository/UsersRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.revature.model.Users;
 
+@Repository(value = "usersRepository")
 public interface UsersRepository extends JpaRepository<Users, Integer> {
 
 	// Adding a new patient or doctor
